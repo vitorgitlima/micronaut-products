9be11ae2398856750ae5ee5d53dbd5ae2007a158
VitorLima <vitorliima18@gmail.com>
2025-11-22 23:36:10 -0300

    feature-web: Criando testes com Junit sobre Query parameters

--- src/test/java/com/vitorlima/udemy/ProductsControllerTest.java
+++ src/test/java/com/vitorlima/udemy/ProductsControllerTest.java
@@ -41,19 +41,40 @@ class ProductsControllerTest {
 
     }
 
-    private String logProducts(JsonNode response) throws IOException {
-
-        return jsonMapper.writeValueAsString(response);
-    }
-
     @Test
     void canFetchProductByID(){
-
-        var response = client.toBlocking().retrieve(("/0"), Product.class);
+        Product response = client.toBlocking().retrieve(("/0"), Product.class);
 
         assertEquals(0, response.id());
         assertEquals(Product.Type.COFFEE, response.type());
         assertNotNull(response.name());
+    }
+
+    @Test
+    void canLimitTheAmountOfProductsToFetchTo5() throws IOException {
+
+        JsonNode response = client.toBlocking().retrieve(("/filter?max=5"), JsonNode.class);
+
+        LOG.debug("Retrieved 5 products: {}", logProducts(response));
+        assertEquals(5, response.size());
+
+    }
+
+    @Test
+    void canFilterUsingOffsetAndMaxLimit() throws IOException {
+
+        JsonNode response = client.toBlocking().retrieve(("/filter?max=2&offset=6"), JsonNode.class);
+
+        LOG.debug("Retrieved 2 products starting with offset 6: {}", logProducts(response));
+        assertEquals(2, response.size());
+        assertEquals(6, response.get(0).get("id").getIntValue());
+        assertEquals(7, response.get(1).get("id").getIntValue());
+
+    }
+
+    private String logProducts(JsonNode response) throws IOException {
+
+        return jsonMapper.writeValueAsString(response);
     }
 
 }
