535af21c5087763c8c50080dde24b9c997bf9215
VitorLima <vitorliima18@gmail.com>
2025-11-23 00:07:55 -0300

    feature-web: Devolvendo status code correto para post created 201. e trando erros de duplicidades de id na criacao, devolvendo mensagem de erro

--- src/main/java/com/vitorlima/udemy/controller/admin/AdminProductsController.java
+++ src/main/java/com/vitorlima/udemy/controller/admin/AdminProductsController.java
@@ -2,20 +2,33 @@ package com.vitorlima.udemy.controller.admin;
 
 import com.vitorlima.udemy.InMemoryStore;
 import com.vitorlima.udemy.domain.Product;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Status;
+import io.micronaut.http.exceptions.HttpStatusException;
 
 @Controller("/admin/products")
 public class AdminProductsController {
 
     private final InMemoryStore store;
 
-    public AdminProductsController(InMemoryStore store) {this.store = store;}
+    public AdminProductsController(InMemoryStore store) {
+        this.store = store;
+    }
 
+    @Status(HttpStatus.CREATED)
     @Post(consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
     public Product addNewProduct(@Body Product product) {
+
+        if (store.getProducts().containsKey(product.id())) {
+            throw new HttpStatusException(
+                    HttpStatus.CONFLICT,
+                    "Product with id " + product.id() + " already exists"
+            );
+        }
 
         return store.addProduct(product);
     }
